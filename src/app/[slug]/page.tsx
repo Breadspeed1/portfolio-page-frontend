367c064d568761da75a01f2919b2a25714b3035c
Aiden Voth <aiden@aidens-pc.localdomain>
2025-01-27 22:32:17 -0500

    begin projects section

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,20 +1,30 @@
 'use client'
 
+import Image from "next/image";
 import Sidebar from "./sidebar";
 import { useEffect, useRef, useState } from 'react';
 
 const TYPE_SPEED = 200;
+const CARAT = '_';
+
+const GITHUB_LINK = "https://github.com/Breadspeed1";
+const LINKDEDIN_LINK = "www.linkedin.com/in/aiden-voth-a0972b334";
 
 function Description(props: { name: string }) {
   const intro = (
-    <div className="font-sans text-8xl text-left ml-5">Hey <b className="break-all">{props.name}</b>, I&apos;m <b>Aiden</b>.</div>
+    <div className="font-sans text-8xl text-left ml-5">Hey <b className="break-all">{props.name}</b>,<br/> I&apos;m <b>Aiden</b>.</div>
   );
 
   const desc = (
-    <div className="font-sans text-3xl text-left ml-5 pt-8">
-      I am curently a student of <b>Software Engineering</b> at Rochester Institute of Technology (<b>RIT</b>).
-      I am a <b>full-stack</b> engineer, and I especially enjoy <b>back-end</b>.
-      My favorite language to work in is <b>Rust</b>.
+    <div className="font-sans text-3xl text-left ml-8 pt-8 flex">
+      <div className="w-2 flex-none bg-[--color-4] mr-4">
+        
+      </div>
+      <div className="flex-1">
+        I am curently a student of <b>Software Engineering</b> at Rochester Institute of Technology (<b>RIT</b>).
+        I am a <b>full-stack</b> engineer, and I especially enjoy <b>back-end</b>.
+        My favorite language to work in is <b>Rust</b>.
+      </div>
     </div>
   )
 
@@ -26,12 +36,29 @@ function Description(props: { name: string }) {
   )
 }
 
+function SocialLink(props: {
+  src: string,
+  alt: string,
+  link: string,
+  w: number,
+  h: number
+}) {
+  return (
+    <a href={props.link}>
+      <Image className="h-20 w-auto" src={props.src} alt={props.alt} width={props.w} height={props.h}/>
+    </a>
+  )
+}
+
 export default function Home({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const aboutMeRef = useRef(null);
+  const socialsRef = useRef(null);
+  const projectsRef = useRef(null);
+
   const [slug, setSlug] = useState("");
 
   useEffect(() => {
@@ -41,7 +68,7 @@ export default function Home({
           let toSet = res.slug.substring(0, i);
   
           if (i < res.slug.length) {
-            toSet += '_';
+            toSet += CARAT;
           }
   
           setTimeout(() => setSlug(toSet), i * TYPE_SPEED)
@@ -50,18 +77,20 @@ export default function Home({
     })
   })
 
-  
-  
-
   //TODO: Integrate with api
   return (
     <div className="flex overflow-hidden">
       <div className="fixed flex-none h-screen">
-        <Sidebar aboutMeRef={aboutMeRef}/>
+        <Sidebar aboutMeRef={aboutMeRef} socialsRef={socialsRef} projectsRef={projectsRef}/>
       </div>
       <div className="flex-1 bg-[--color-2] p-10 ml-20">
         <div ref={aboutMeRef}></div>
         <Description name={slug} />
+        <div className="flex justify-evenly items-center ml-24 mr-24 mt-24" ref={socialsRef}>
+          <SocialLink w={64} h={64} src={"/github-mark-white.png"} alt="github cat" link={GITHUB_LINK} />
+          <SocialLink w={256} h={256} src={"/LI-Logo.png"} alt="linkedin logo" link={LINKDEDIN_LINK} />
+        </div>
+        <div className="font-sans text-8xl ml-5 mt-24" ref={projectsRef}><b>Projects</b></div>
       </div>
     </div>
   );
--- src/app/[slug]/sidebar.tsx
+++ src/app/[slug]/sidebar.tsx
@@ -8,18 +8,22 @@ function MenuItem({ text, scrollRef }: { text: string, scrollRef: RefObject<HTML
     }
     
     return (
-        <div onClick={scrollTo} className={"overflow-hidden group-hover:opacity-100 opacity-0 ease-in-out duration-300"}>
+        <div onClick={scrollTo} className={"cursor-pointer select-none overflow-hidden group-hover:opacity-100 opacity-0 ease-in-out duration-300"}>
             <div className="font-sans text-2xl text-[--background] bg-transparent hover:bg-[--color-4] duration-200 ease-in-out h-20 flex justify-center items-center">{text}</div>
         </div>
     )
 }
 
 export default function Sidebar(props: {
-    aboutMeRef: RefObject<HTMLElement | null>
+    aboutMeRef: RefObject<HTMLElement | null>,
+    socialsRef: RefObject<HTMLElement | null>,
+    projectsRef: RefObject<HTMLElement | null>
 }) {
     return (
         <div className="group font-sans w-24 h-screen m-0 bg-[--foreground] hover:w-64 opacity-15 hover:opacity-100 duration-300 ease-in-out">
             <MenuItem text="About Me" scrollRef={props.aboutMeRef}/>
+            <MenuItem text="Socials" scrollRef={props.socialsRef}/>
+            <MenuItem text="Projects" scrollRef={props.projectsRef}/>
         </div>
     );
 };
