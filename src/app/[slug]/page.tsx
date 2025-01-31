4f62ad2124ede2a74e05f5e211e72f654351ce54
Aiden Voth <aiden@aidens-pc.>
2025-01-30 20:13:20 -0500

    fix mobile :)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -45,15 +45,15 @@ export default async function Home({
           { id: "projects", title: "Projects" }
         ]}/>
       </div>
-      <div className="flex flex-1 flex-col justify-center items-center bg-[--color-2]q h-auto">
-        <div id="about-me" className="flex-1 lg:w-[50vw] h-auto  w-full">
+      <div className="flex flex-1 flex-col justify-center items-center bg-[--color-2]q h-auto m-5 md:ml-0 md:mr-0">
+        <div id="about-me" className="flex-1 lg:w-[50vw] md:w-[75vw] h-auto  w-full">
           <Description name={name} />
         </div>
         <div id="socials" className="flex justify-evenly items-center mt-24 w-full">
           <SocialLink w={64} h={64} src={"/github-mark-white.png"} alt="github cat" link={GITHUB_LINK} />
           <SocialLink w={256} h={256} src={"/LI-Logo.png"} alt="linkedin logo" link={LINKDEDIN_LINK} />
         </div>
-        <div id="projects flex-none" className="font-sans text-4xl md:text-7x mt-24 flex-1 lg:w-[50vw] w-full"><b>Projects</b></div>
+        <div id="projects flex-none" className="font-sans text-4xl md:text-7x mt-24 flex-1 md:w-[75vw] lg:w-[50vw] w-full"><b>Projects</b></div>
       </div>
     </div>
   );
