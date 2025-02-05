5a7c23f89c676a9eca7f52e0ad14e3b29b6b191d
Aiden Voth <aiden@aidens-pc.>
2025-02-04 21:23:53 -0500

    resume link

--- src/app/[slug]/socials.tsx
+++ src/app/[slug]/socials.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
-import { Section, Heading, Separator, Flex, IconButton } from "@radix-ui/themes";
+import { FileTextIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Section, Heading, Separator, Flex, IconButton, Tooltip } from "@radix-ui/themes";
 
 const GITHUB_LINK = "https://github.com/Breadspeed1";
 const LINKDEDIN_LINK = "https://linkedin.com/in/aiden-voth-a0972b334";
+const RESUME_LINK = "https://docs.google.com/document/d/1Res_po3ysesxdx-b7ChpiR7nbEQbNKYid7oQTe9Wr-c/edit?usp=sharing";
 
 export default function Socials() {
     const open = (link: string) => window.open(link, '_blank')?.focus()
@@ -14,12 +15,21 @@ export default function Socials() {
             <Heading size="9">Socials</Heading>
             <Separator my="2" size="4"/>
             <Flex justify="center" gapX="9" align="center" my="4">
-                <IconButton onClick={() => open(LINKDEDIN_LINK)} variant="ghost" >
-                    <LinkedInLogoIcon cursor="pointer" width="48" height="48"/>
-                </IconButton>
-                <IconButton onClick={() => open(GITHUB_LINK)} variant="ghost" radius="full">
-                    <GitHubLogoIcon width="48" cursor="pointer" height="48"/>
-                </IconButton>
+                <Tooltip content="View My Linkedin!">
+                    <IconButton onClick={() => open(LINKDEDIN_LINK)} variant="ghost" >
+                        <LinkedInLogoIcon cursor="pointer" width="48" height="48"/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip content="View My Github!">
+                    <IconButton onClick={() => open(GITHUB_LINK)} variant="ghost" radius="full">
+                        <GitHubLogoIcon width="48" cursor="pointer" height="48"/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip content="View My Resume!">
+                    <IconButton onClick={() => open(RESUME_LINK)} variant="ghost" radius="medium">
+                        <FileTextIcon width="48" cursor="pointer" height="48"/>
+                    </IconButton>
+                </Tooltip>
             </Flex>
         </Section>
     </>
