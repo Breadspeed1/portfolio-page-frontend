3b7982b3ceb111e4bd254873b69e3a2be8189406
Aiden Voth <aiden@aidens-pc.>
2025-02-12 16:51:24 -0500

    fix mobile view of project pages

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -22,8 +22,8 @@ export default async function ProjectPage({
             </Card>
         </Flex>
         <Flex width="100%" direction="column" align="center">
-            <Heading wrap="wrap" size="9" mt="9">{proj.title}</Heading>
-            <Heading wrap="wrap" weight="medium" size="6" mt="4">{proj.subtitle}</Heading>
+            <Heading mx="3" wrap="wrap" size="9" mt="9" align="center">{proj.title}</Heading>
+            <Heading mx="3" wrap="wrap" weight="medium" size="6" mt="4" align="center">{proj.subtitle}</Heading>
             <Separator size="4" my="5"/>
             <Flex mx="6" flexShrink="1" width={{ sm: "80vw", lg: "50vw", md: "60vw", xl: "40vw" }} justify="center" direction="column">
                 {proj.sections.map((sec) => {
